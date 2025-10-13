5af8086a2e73d55ddbf3b25ed815c2ca0795d9e2
Zealousideal <desinoy2018@gmail.com>
2025-10-13 20:08:34 +0530

    Neural network Basic Structure Added. The car works and drives itslef. Sort of.

--- car.js
+++ car.js
@@ -13,9 +13,15 @@ class Car {
 
     this.damaged = false;
 
+    this.useBrain = controlType == "AI";
+
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+
+      // First layer is all of the rays, second is the hidden layer and the third is the output layer with 4 neurons, one for each direction.
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
+
     this.controls = new Controls(controlType);
   }
 
@@ -28,6 +34,20 @@ class Car {
 
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset
+      );
+
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+      console.log(outputs);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
 
---
+++ network.js
@@ -0,0 +1,70 @@
+class NeuralNetwork {
+  constructor(neuronCounts) {
+    this.levels = [];
+    for (let i = 0; i < neuronCounts.length - 1; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForward(givenInputs, network) {
+    let outputs = Level.feedForward(givenInputs, network.levels[0]);
+
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs = Level.feedForward(outputs, network.levels[i]);
+    }
+
+    return outputs;
+  }
+}
+
+class Level {
+  constructor(inputCount, outputCount) {
+    this.inputs = new Array(inputCount);
+    this.outputs = new Array(outputCount);
+    this.biases = new Array(outputCount);
+
+    this.weights = [];
+    // For each neuron make an array consisting of the weights from each neuron to each of the outputs
+    for (let i = 0; i < inputCount; i++) {
+      this.weights[i] = new Array(outputCount);
+    }
+
+    Level.#randomize(this);
+  }
+
+  //   For the time being randomizing the inputs and the outputs to make it "just work"
+  static #randomize(level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      for (let j = 0; j < level.outputs.length; j++) {
+        level.weights[i][j] = Math.random() * 2 - 1; // Random value between -1 and 1
+      }
+    }
+
+    for (let i = 0; i < level.biases.length; i++) {
+      level.biases[i] = Math.random() * 2 - 1;
+    }
+  }
+
+  //   This is the basics of how to get from one layer to another
+  static feedForward(givenInputs, level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i];
+    }
+
+    for (let i = 0; i < level.outputs.length; i++) {
+      let sum = 0;
+
+      for (let j = 0; j < level.inputs.length; j++) {
+        sum += level.inputs[j] * level.weights[j][i];
+      }
+
+      if (sum > level.biases[i]) {
+        level.outputs[i] = 1;
+      } else {
+        level.outputs[i] = 0;
+      }
+    }
+
+    return level.outputs;
+  }
+}
